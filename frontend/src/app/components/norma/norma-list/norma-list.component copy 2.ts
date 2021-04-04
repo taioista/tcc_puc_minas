c529aa299c1b11b8b93c435c7613a4387d6306c0
Pedro Pires <pedropires@Pedros-MacBook-Air.local>
2021-04-04 10:12:15 -0300

    Inclusão do componente de listar normas

---
+++ frontend/src/app/components/norma/norma-list/norma-list.component copy 2.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-norma-list',
+  templateUrl: './norma-list.component.html',
+  styleUrls: ['./norma-list.component.css']
+})
+export class NormaListComponent {
+  title = 'SIGO';
+}
